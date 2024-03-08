86796c7c831db5552bfb85292f0424130a6a44ce
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:22:29 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,9 @@
+11.// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+ let name1: string[] = ["uzma", "kainat", "kiran", "dr.murk", "farhana"];
+
+ console.log(name1[0]);
+ console.log(name1[1]);
+ console.log(name1[2]);
+ console.log(name1[3]);
+ console.log(name1[4]);
+ 
